378e2993de5a0da4ba9d78c3cd888e8113a480b1
Bronstrom <bafellstrom@gmail.com>
2018-12-11 03:18:51 -0500

    Fixed board, got rid of unnecessary player seclection screen

--- Graphics.py
+++ Graphics.py
@@ -4,6 +4,8 @@ from kivy.lang import Builder
 from kivy.properties import StringProperty    # used to change text
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition, SlideTransition    # ScreenManager: keeps track of screens,
 # Screen allow for the creation of screens
+from kivy.clock import Clock
+
 import PlayGameConsole
 
 # Configure input
@@ -19,182 +21,139 @@ class TitleScreen(Screen):
 
     :param Screen: Screen in GUI. Tasked by screen manager.
     """
-    pass
 
     game = " "
 
-    """"
-    Sets game choice in PlayGameConsole utility class.
-    """
     def game_choice(self):
-        global game
+        """"
+        Sets game choice in PlayGameConsole utility class.
+        """
         PlayGameConsole.GameConsole.game_choice(game)
         print("game: " + game)
 
-    """"
-    Sets game to "Person vs Person" depending if button is pressed with title.
-    """
     def p_v_p(self):
+        """"
+        Sets game to "Person vs Person" depending if button is pressed with title.
+        """
         global game
         game = "player_vs_player"
         self.game_choice()
 
-    """"
-    Sets game to Person vs Computer depending if button is pressed with title.
-    """
     def p_v_c(self):
+        """"
+        Sets game to Person vs Computer depending if button is pressed with title.
+        """
         global game
         game = "player_vs_computer"
         self.game_choice()
 
-    """"
-    Sets game to Computer vs Computer depending if button is pressed with title.
-    """
     def c_v_c(self):
+        """"
+        Sets game to Computer vs Computer depending if button is pressed with title.
+        """
         global game
         game = "computer_vs_computer"
         self.game_choice()
 
 
-class ChooseScreen(Screen):
-    pass
-
-    player_one = " "
-    player_two = " "
-    computer_player = " "
-    player_two_exist = True
-    player_one_pick = True
-    label = StringProperty("Player 1 Choose")
-
-    def set_player_two(self):
-        print("your game: " + PlayGameConsole.GameConsole.game_choice(game))
-        return PlayGameConsole.GameConsole.game_choice(game) == "player_vs_player"
-
-    def check_pick(self, player_one, player_two):
-        if self.set_player_two():
-            if self.player_one_pick:
-                self.player_one_pick = False
-                PlayGameConsole.GameConsole.set_player_one(self, player_one)
-                self.label = str("Player 2 Choose")
-                self.ids.label_choose.text = self.label
-            else:
-                if self.manager.current == "Choose":
-                    PlayGameConsole.GameConsole.set_player_two(self, player_two)
-                    self.manager.transition = FadeTransition()
-                    self.manager.current = "Game"
-                else:
-                    self.manager.current == "Choose"
-        else:
-            if player_one == "X":
-                computer_player = "O"
-                print("CPU: " + computer_player)
-                self.manager.current = "Game"
-            else:
-                computer_player = "X"
-                print("CPU: " + computer_player)
-                self.manager.current = "Game"
-                return player_one
-                return computer_player
-
-    def x_button_press(self):
-        if self.player_one_pick:
-            player_one = "X"
-            print("player one: " + player_one)
-            self.ids.X_Button.color = 1, 1, 1, 0.1
-            self.check_pick(player_one, self.player_two)
-        elif not self.player_one == "X":
-            player_two = "X"
-            print("player two: " + player_two)
-            self.check_pick(self.player_one, player_two)
-        else:
-            print("'X' already selected")
-
-    def o_button_press(self):
-        if self.player_one_pick:
-            player_one = "O"
-            print("player one: " + player_one)
-            self.ids.O_Button.color = 1, 1, 1, 0.1
-            self.check_pick(player_one, self.player_two)
-        elif not self.player_one == "O":
-            player_two = "O"
-            print("player two: " + player_two)
-            self.check_pick(self.player_one, player_two)
-        else:
-            print("'O' already selected")
-
-
 class GameScreen(Screen):
-    pass
 
     position = None
     player_number = 1
     player_one_win = False
     player_two_win = False
-    player_one = " "
-    player_two = " "
+
+    def clear_btns(self):
+        self.ids.btn0.text = " "
+        self.ids.btn1.text = " "
+        self.ids.btn2.text = " "
+        self.ids.btn3.text = " "
+        self.ids.btn4.text = " "
+        self.ids.btn5.text = " "
+        self.ids.btn6.text = " "
+        self.ids.btn7.text = " "
+        self.ids.btn8.text = " "
 
     def check_win(self):
-        if self.player_one_win:
+        if self.player_one_win or self.player_two_win:
+            WinScreen.label_change(self.player_one_win, self.player_two_win)
             self.manager.current = "Win"
-        if self.player_two_win:
-            self.manager.current = "Win"
+
+    def set_piece(self):
+        if self.player_number == 1:
+            return PlayGameConsole.GameConsole.player_two
+        if self.player_number == 2:
+            return PlayGameConsole.GameConsole.player_one
 
     def btn0(self):
         position = 0
         PlayGameConsole.GameConsole.update_board(self.player_number, position)
+        self.ids.btn0.text = self.set_piece()
         self.check_win()
 
     def btn1(self):
         position = 1
         PlayGameConsole.GameConsole.update_board(self.player_number, position)
+        self.ids.btn1.text = self.set_piece()
         self.check_win()
 
     def btn2(self):
         position = 2
         PlayGameConsole.GameConsole.update_board(self.player_number, position)
+        self.ids.btn2.text = self.set_piece()
         self.check_win()
 
     def btn3(self):
         position = 3
         PlayGameConsole.GameConsole.update_board(self.player_number, position)
+        self.ids.btn3.text = self.set_piece()
         self.check_win()
 
     def btn4(self):
         position = 4
         PlayGameConsole.GameConsole.update_board(self.player_number, position)
+        self.ids.btn4.text = self.set_piece()
         self.check_win()
 
     def btn5(self):
         position = 5
         PlayGameConsole.GameConsole.update_board(self.player_number, position)
+        self.ids.btn5.text = self.set_piece()
         self.check_win()
 
     def btn6(self):
         position = 6
         PlayGameConsole.GameConsole.update_board(self.player_number, position)
+        self.ids.btn6.text = self.set_piece()
         self.check_win()
 
     def btn7(self):
         position = 7
         PlayGameConsole.GameConsole.update_board(self.player_number, position)
+        self.ids.btn7.text = self.set_piece()
         self.check_win()
 
     def btn8(self):
         position = 8
         PlayGameConsole.GameConsole.update_board(self.player_number, position)
+        self.ids.btn8.text = self.set_piece()
         self.check_win()
 
 
 class WinScreen(Screen):
-    pass
 
-    label = StringProperty("Cats game!")
+    label = str(" ")
 
-    def player_win(self):
-        if GameScreen.player_one_win:
-            self.label = str("Player 1 Wins!")
-        if GameScreen.player_two_win:
-            self.label = str("Player 2 Wins!")
+    @staticmethod
+    def label_change(player_one_win, player_two_win):
+        global label
+        if player_one_win:
+            label = str("O's win!")
+        elif player_two_win:
+            label = str("X's win!")
+        else:
+            label = str("Cats game!")
+        return label
 
 
 class GraphicsApp(App):
@@ -210,7 +169,6 @@ class GraphicsApp(App):
 
         sm = ScreenManager(transition=transition(count))
         sm.add_widget(TitleScreen(name="Title"))
-        sm.add_widget(ChooseScreen(name="Choose"))
         sm.add_widget(GameScreen(name="Game"))
         sm.add_widget(WinScreen(name="Win"))
 
--- PlayGameConsole.py
+++ PlayGameConsole.py
@@ -4,29 +4,18 @@ import Graphics
 
 class GameConsole:
 
+    player_one = "X"
+    player_two = "O"
     set_game = " "
-    set_player_one = " "
-    set_player_two = " "
 
     @staticmethod
     def game_choice(game):
         global set_game
         set_game = game
-        return game
+        return set_game
+        self.board()
 
-    def set_player_one(self, player_one):
-        if set_game == "player_vs_player":
-            self.set_player_one = player_one
-            print(player_one)
-        return player_one
-
-    def set_player_two(self, player_two):
-        if set_game == "player_vs_player":
-            self.set_player_two = player_two
-            print(player_two)
-        return player_two
-
-    game = GameBoard(set_player_one, set_player_two)
+    game = GameBoard(player_one, player_two)
     game.print_board()
 
     @staticmethod
@@ -41,9 +30,9 @@ class GameConsole:
                     Graphics.GameScreen.player_number = 2
                 else:
                     Graphics.GameScreen.player_number = 1
-            if GameConsole.game.check_win(1):
-                Graphics.GameScreen.player_one_win = True
-            if GameConsole.game.check_win(2):
-                Graphics.GameScreen.player_two_win = True
+        if GameConsole.game.check_win(1):
+            Graphics.GameScreen.player_one_win = True
+        if GameConsole.game.check_win(2):
+            Graphics.GameScreen.player_two_win = True
 
         GameConsole.game.print_board()
